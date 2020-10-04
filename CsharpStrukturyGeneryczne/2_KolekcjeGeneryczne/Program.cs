34f8b193a3f6cfeb4f4c4a312c19ba62a0d9b945
Karol Króliczek <15zajc15@gmail.com>
2020-10-04 18:59:52 +0200

    HashSet_plus_testy

--- CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CsharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -10,20 +10,50 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
-            Queue<Pracownik> kolejka = new Queue<Pracownik>();
-            kolejka.Enqueue(new Pracownik() { Imie = "Karol", Nazwisko = "Króliczek", Wiek = 30, Wzrost = 188 });
-            kolejka.Enqueue(new Pracownik() { Imie = "Maciej", Nazwisko = "Zygmunt", Wiek = 30, Wzrost = 183 });
-            kolejka.Enqueue(new Pracownik() { Imie = "Olaf", Nazwisko = "Pepito", Wiek = 23, Wzrost = 178 });
+            SprawdzenieKolejki();
+            SprawdzenieStosu();
+            SprawdzenieHashSetu();
 
-            while(kolejka.Count > 0)
+            Console.ReadLine();
+        }
+
+        private static void SprawdzenieHashSetu()
+        {
+            HashSet<int> liczbyHashSet = new HashSet<int>();
+            liczbyHashSet.Add(2);
+            liczbyHashSet.Add(4);
+            liczbyHashSet.Add(6);
+            liczbyHashSet.Add(8);
+            liczbyHashSet.Add(4);
+            liczbyHashSet.Add(6);
+
+            foreach (var item in liczbyHashSet)
             {
-                var item = kolejka.Dequeue();
-                Console.WriteLine(item.Imie + " ma " + item.Wzrost + " wzrostu.");
-                
+                Console.WriteLine(item);
             }
-
             Console.WriteLine("");
 
+            HashSet<Pracownik> pracownicyHS = new HashSet<Pracownik>();
+            Pracownik pracownik1 = new Pracownik() { Imie = "Tomasz", Nazwisko = "Komoran", Wiek = 44, Wzrost = 198 };
+            Pracownik pracownik2 = new Pracownik() { Imie = "Artur", Nazwisko = "Burek", Wiek = 24, Wzrost = 168 };
+
+            var dodano1 = pracownicyHS.Add(pracownik1);
+            pracownicyHS.Add(pracownik2);
+            var dodano2 = pracownicyHS.Add(pracownik1);
+
+            Console.WriteLine(dodano1);
+            Console.WriteLine(dodano2);
+            Console.WriteLine("");
+
+            foreach (var item in pracownicyHS)
+            {
+                Console.WriteLine(item.Imie + " " + item.Nazwisko);
+            }
+            Console.WriteLine("");
+        }
+
+        private static void SprawdzenieStosu()
+        {
             Stack<Pracownik> stos = new Stack<Pracownik>();
             stos.Push(new Pracownik() { Imie = "Karol", Nazwisko = "Króliczek", Wiek = 30, Wzrost = 188 });
             stos.Push(new Pracownik() { Imie = "Maciej", Nazwisko = "Zygmunt", Wiek = 30, Wzrost = 183 });
@@ -35,8 +65,23 @@ namespace _2_KolekcjeGeneryczne
                 Console.WriteLine(item.Imie + " ma " + item.Wzrost + " wzrostu.");
 
             }
+            Console.WriteLine("");
+        }
 
-            Console.ReadLine();
+        private static void SprawdzenieKolejki()
+        {
+            Queue<Pracownik> kolejka = new Queue<Pracownik>();
+            kolejka.Enqueue(new Pracownik() { Imie = "Karol", Nazwisko = "Króliczek", Wiek = 30, Wzrost = 188 });
+            kolejka.Enqueue(new Pracownik() { Imie = "Maciej", Nazwisko = "Zygmunt", Wiek = 30, Wzrost = 183 });
+            kolejka.Enqueue(new Pracownik() { Imie = "Olaf", Nazwisko = "Pepito", Wiek = 23, Wzrost = 178 });
+
+            while (kolejka.Count > 0)
+            {
+                var item = kolejka.Dequeue();
+                Console.WriteLine(item.Imie + " ma " + item.Wzrost + " wzrostu.");
+
+            }
+            Console.WriteLine("");
         }
     }
 }
---
+++ CsharpStrukturyGeneryczne/UnitTestTypyGeneryczne/HashSet.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestTypyGeneryczne
+{
+    [TestClass]
+    public class HashSet
+    {
+        [TestMethod]
+        public void IntersectWithTest()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.IntersectWith(set2);
+
+            Assert.AreEqual(2, set1.Count);
+        }
+
+        [TestMethod]
+        public void SymmetricExceptWithTest()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.SymmetricExceptWith(set2);
+
+            Assert.AreEqual(2, set1.Count);
+            Assert.IsTrue(set1.Contains(1));
+            Assert.IsTrue(set1.Contains(4));
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 3, 4, 5 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.RemoveWhere(x => x > 2);
+
+            Assert.AreEqual(2, set1.Count);
+            Assert.IsTrue(!set1.Contains(4));
+        }
+
+        [TestMethod]
+        public void RemoveTest()
+        {
+            var set1 = new HashSet<int>() { 1, 2, 8, 4, 5 };
+            var set2 = new HashSet<int>() { 2, 3, 4 };
+
+            set1.Remove(8);
+
+            Assert.AreEqual(4, set1.Count);
+            Assert.IsTrue(!set1.Contains(8));
+        }
+    }
+}
